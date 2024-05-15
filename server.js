295ff8d99b3e3e733ff326fb68065d4ef1a412da
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-15 12:55:13 -0500

    filename and dirname added

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+// get file name
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
   try {
     if (req.method === 'GET') {
+      let filePath;
       if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Homepage</h1>');
+        filePath.join('__dirname', 'public', 'index.html');
       } else if (req.url === '/about-us') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>About Us</h1>');
+        filePath.join('__dirname', 'public', 'about.html');
       } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>Not found</h1>');
+        throw new Error('Not Found');
       }
+      const data = await fs.readFile(filePath);
+      res.setHeader(200, 'Content-type', 'text/html');
+      res.write(data);
+      res.end();
     } else {
       throw new Error('Method Not Allowed');
     }
