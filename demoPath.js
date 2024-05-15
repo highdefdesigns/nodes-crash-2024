f91681c38b0d29a7bb5f14ac1c7b18f473af1da2
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-15 16:53:16 -0500

    path demo

--- demoPath.js
+++ demoPath.js
@@ -1,5 +1,27 @@
 import path from 'path';
+// console.log(path);
+import url from 'url';
 
-console.log(path);
+const filePath = './dir1/dir2/test.txt ';
 
-const filePath = './dir1/dir2/test.txt';
+// baseName()
+console.log(path.basename(filePath));
+
+// directoryName()
+console.log(path.dirname(filePath));
+
+// extName()
+console.log(path.extname(filePath));
+
+// parse()
+console.log(path.parse(filePath));
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname, __filename);
+
+// join()
+// resolve() does exacly like join()
+const filePath2 = path.join(__dirname, 'dir1', 'dir2', 'test.txt');
+console.log(filePath2);
