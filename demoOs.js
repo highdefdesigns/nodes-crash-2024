67e94c8be2446bf0b94ccc3b3e56ccecfe746056
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-15 21:47:32 -0500

    os demo

---
+++ demoOs.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// userInfo()
+// console.log(os.userInfo());
+
+// totalmem() - in bytes
+// console.log(os.totalmem());
+
+// freemem() - in bytes
+// console.log(os.freemem());
+
+// cpus()
+console.log(os.cpus());
